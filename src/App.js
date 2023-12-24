5b2c704f7e1de8767e2df1080e527bc9f2e99b4b
David Trinidad <davidtrinidad016@gmail.com>
2023-12-24 09:54:34 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
 import './App.css';
+import Boton from './componentes/Boton';
+import Pantalla from './componentes/Pantalla';
+import BotonClear from './componentes/BotonClear';
+import {useState} from 'react';
+import {evaluate} from 'mathjs';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [input, setInput] = useState('');
+
+	const agregarInput = (val) => {
+		// Verificar si el input está vacío y el valor actual es un operador o un punto
+		if (!input && (val === '.' || /[+\-.*/]/.test(val))) {
+			return;
+		}
+		// Verificar si el último carácter es un operador
+		const ultimoCaracterEsOperador = /[+\-.*/]$/.test(input);
+
+		// Verificar si el valor actual es un operador
+		const esOperador = /[+\-.*/]/.test(val);
+
+		// Verificar si el valor actual es un punto
+		const esPunto = val === '.';
+
+		// Verificar si el último carácter es un número
+		const ultimoCaracterEsNumero = /\d$/.test(input);
+
+		// Verificar si el último carácter es un operador seguido de un número
+		const ultimoCaracterEsOperadorYNumero = /[+\-*/]\d$/.test(input);
+
+		if (ultimoCaracterEsOperador && esOperador) {
+			return;
+		}
+
+		if (
+			esPunto &&
+			!ultimoCaracterEsNumero &&
+			!ultimoCaracterEsOperadorYNumero
+		) {
+			return;
+		}
+
+		// Concatenar el valor al estado
+		setInput(input + val);
+	};
+
+	const calcularResultado = () => {
+		const ultimoCaracterEsOperador = /[+\-.*/]$/.test(input);
+
+		// Verificar si hay operaciones válidas en la cadena de entrada
+		const hayOperacionesValidas = /(\d+[+\-.*/]\d+)$/.test(input);
+
+		if (
+			!ultimoCaracterEsOperador &&
+			hayOperacionesValidas &&
+			input.trim() !== ''
+		) {
+			if (/[+\-*/]/.test(input)) {
+				setInput(evaluate(input));
+			}
+		}
+	};
+
+	return (
+		<div className='App'>
+			<div className='freecodecamp-logo-contenedor'>
+				<h1 className='titulo-calculadora'>CALCULADORA</h1>
+			</div>
+			<div className='contenedor-calculadora'>
+				<Pantalla input={input} />
+				<div className='fila'>
+					<Boton manejarClick={agregarInput}>1</Boton>
+					<Boton manejarClick={agregarInput}>2</Boton>
+					<Boton manejarClick={agregarInput}>3</Boton>
+					<Boton manejarClick={agregarInput}>+</Boton>
+				</div>
+				<div className='fila'>
+					<Boton manejarClick={agregarInput}>4</Boton>
+					<Boton manejarClick={agregarInput}>5</Boton>
+					<Boton manejarClick={agregarInput}>6</Boton>
+					<Boton manejarClick={agregarInput}>-</Boton>
+				</div>
+				<div className='fila'>
+					<Boton manejarClick={agregarInput}>7</Boton>
+					<Boton manejarClick={agregarInput}>8</Boton>
+					<Boton manejarClick={agregarInput}>9</Boton>
+					<Boton manejarClick={agregarInput}>*</Boton>
+				</div>
+				<div className='fila'>
+					<Boton manejarClick={calcularResultado}>=</Boton>
+					<Boton manejarClick={agregarInput}>0</Boton>
+					<Boton manejarClick={agregarInput}>.</Boton>
+					<Boton manejarClick={agregarInput}>/</Boton>
+				</div>
+				<div className='fila'>
+					<BotonClear manejarClear={() => setInput('')}>Clear</BotonClear>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/componentes/LogoFreeCodeCamp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../App.css';
+
+function LogoFreeCodeCamp({Logo}) {
+	<div className='freecodecamp-logo-contenedor'>
+		<img src={Logo} className='freecodecamp-logo' alt='Logo de freecodecamp' />
+	</div>;
+}
+export default LogoFreeCodeCamp;
